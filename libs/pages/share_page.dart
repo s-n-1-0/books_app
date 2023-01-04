17c1af453f526e4453ab50f2cc1e57f93aa8cdda
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-04 15:54:23 +0900

    共有画面レイアウトを追加

---
+++ libs/pages/share_page.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ShareBooksShareQuery {
+  String isbn;
+
+  /// 空文字の場合は未指定(=openbd) "openbd" | googlebooks
+  String from;
+
+  /// 空文字の場合は未指定
+  String comment;
+
+  ShareBooksShareQuery({required this.isbn, this.from = "", this.comment = ""});
+
+  final String _sharePageUrl = "https://books.sn-10.net/ja/share";
+  String getBookDataUrl() {
+    var url = "$_sharePageUrl?";
+    url += "isbn=${Uri.encodeFull(isbn)}";
+    if (from != "") url += "&from=${Uri.encodeFull(from)}";
+    if (comment != "") url += "&comment=${Uri.encodeComponent(comment)}";
+    return url;
+  }
+}
+
+class SharePage extends StatelessWidget {
+  SharePage({super.key, required this.query});
+  ShareBooksShareQuery query;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: query.getBookDataUrl(),
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
