9736c1ba2f376647533a1b8eac64fd2e6f11733c
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-08 01:28:22 +0900

    Twitter共有処理はアプリ側で行うように修正

--- libs/components/common_webview.dart
+++ libs/components/common_webview.dart
@@ -1,5 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CommonWebView extends StatelessWidget {
   const CommonWebView(this.url, {super.key});
@@ -7,10 +9,32 @@ class CommonWebView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(url)),
-        initialOptions: InAppWebViewGroupOptions(
-            android: AndroidInAppWebViewOptions(
-                mixedContentMode:
-                    AndroidMixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW)));
+      initialUrlRequest: URLRequest(url: Uri.parse(url)),
+      initialOptions: InAppWebViewGroupOptions(
+          android: AndroidInAppWebViewOptions(
+              mixedContentMode:
+                  AndroidMixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW)),
+      onWebViewCreated: (controller) {
+        controller.addJavaScriptHandler(
+          handlerName: 'completedSharing',
+          callback: (args) {
+            final res = args.firstOrNull;
+            if (res != null) {
+              switch (res["type"] ?? "") {
+                case "twitter":
+                  final String twitterUrl = res["url"] ?? "";
+                  if (twitterUrl != "" &&
+                      twitterUrl.startsWith("https://twitter.com/share")) {
+                    launchUrl(Uri.parse(twitterUrl));
+                  }
+                  break;
+                case "default":
+                  break;
+              }
+            }
+          },
+        );
+      },
+    );
   }
 }
