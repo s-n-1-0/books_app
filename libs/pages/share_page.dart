b5f6c0837a80fa58b22f8cc13ba20f2bfc797dd2
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-07-25 00:26:57 +0900

    SharePageのBookDataのUrl生成クラスを再利用できるように移動

--- libs/pages/share_page.dart
+++ libs/pages/share_page.dart
@@ -3,29 +3,9 @@ import 'package:flutter/material.dart';
 import '../components/common_webview.dart';
 import '../utils/web.dart';
 
-class ShareBooksShareQuery {
-  String isbn;
-
-  /// 空文字の場合は未指定(=openbd) "openbd" | googlebooks
-  String from;
-
-  /// 空文字の場合は未指定
-  String comment;
-
-  ShareBooksShareQuery({required this.isbn, this.from = "", this.comment = ""});
-
-  final String _sharePageUrl = "$webOrigin/ja/share";
-  String getBookDataUrl() {
-    var url = "$_sharePageUrl?isbn=${Uri.encodeFull(isbn)}";
-    if (from != "") url += "&from=${Uri.encodeFull(from)}";
-    if (comment != "") url += "&comment=${Uri.encodeComponent(comment)}";
-    return url;
-  }
-}
-
 class SharePage extends StatelessWidget {
   const SharePage({super.key, required this.query});
-  final ShareBooksShareQuery query;
+  final BaseBookData query;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- libs/utils/web.dart
+++ libs/utils/web.dart
@@ -1,1 +1,21 @@
-const webOrigin = "https://books.sn-10.net";
+const webOrigin = "http://192.168.2.105:3000";
+
+//共有に必要な最低限のBookData
+class BaseBookData {
+  BaseBookData({required this.isbn, this.from = "", this.comment = ""});
+  String isbn;
+
+  /// 空文字の場合は未指定(=openbd) "openbd" | googlebooks
+  String from;
+
+  /// 空文字の場合は未指定
+  String comment;
+
+  final String _sharePageUrl = "$webOrigin/ja/share";
+  String getBookDataUrl() {
+    var url = "$_sharePageUrl?isbn=${Uri.encodeFull(isbn)}";
+    if (from != "") url += "&from=${Uri.encodeFull(from)}";
+    if (comment != "") url += "&comment=${Uri.encodeComponent(comment)}";
+    return url;
+  }
+}
