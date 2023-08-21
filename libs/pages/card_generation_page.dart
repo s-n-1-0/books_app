28aca3bf8c4125a93bb7ed71badbdc005e806682
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-22 03:13:00 +0900

    書籍カード共有・アルバム保存処理の追加

--- libs/pages/card_generation_page.dart
+++ libs/pages/card_generation_page.dart
@@ -1,7 +1,9 @@
 import 'dart:typed_data';
 import 'package:share_plus/share_plus.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:flutter/material.dart';
 import '../components/book_card.dart';
+import '../components/elevated_gradation_Button.dart';
 import '../utils/widget2image.dart';
 
 class CardGenerationPage extends StatefulWidget {
@@ -20,6 +22,8 @@ class _CardGenerationPageState extends State<CardGenerationPage> {
 
   Uint8List? _bytes;
   bool isThumbnail = true;
+  ProcessingStatus saveStatus = ProcessingStatus.unprocessed;
+  ProcessingStatus shareStatus = ProcessingStatus.unprocessed;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,56 +33,122 @@ class _CardGenerationPageState extends State<CardGenerationPage> {
                 width: double.infinity,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    if (_bytes != null)
-                      LayoutBuilder(builder: (context, size) {
-                        return ConstrainedBox(
-                            constraints: const BoxConstraints(maxHeight: 500),
-                            child: Container(
-                              padding: const EdgeInsets.all(32),
-                              width: double.infinity,
-                              height: size.maxWidth / 1.775,
-                              child: FittedBox(
-                                  fit: BoxFit.contain,
-                                  child: Image.memory(
-                                      _bytes!.buffer.asUint8List())),
-                            ));
-                      }),
-                    if (_bytes != null)
-                      FloatingActionButton(
-                        onPressed: () {
-                          if (_bytes != null) {
-                            Share.shareXFiles([
-                              XFile.fromData(_bytes!, mimeType: 'image/png')
-                            ]);
-                          }
-                        },
-                        child: const Icon(Icons.save_alt),
-                        backgroundColor: Colors.red,
-                      ),
-                    const SizedBox(height: 35.0),
-                    if (_bytes != null) Divider(),
-                    ElevatedButton(
-                      child: const Text("画像を再生成"),
-                      onPressed: () async {
-                        makeImage(0);
-                      },
-                    ),
-                    //サムネイルURLがそもそも存在しないならオプションを表示しない
-                    if (widget.bookData.coverUrl != "")
-                      SwitchListTile(
-                        title: const Text('サムネイルを表示'),
-                        value: isThumbnail,
-                        onChanged: ((newVal) {
-                          setState(() {
-                            isThumbnail = newVal;
-                          });
-                          makeImage(0);
-                        }),
-                        secondary: const Icon(Icons.image_rounded),
-                      )
-                  ],
+                  children: buildAfterGeneration() +
+                      [
+                        ElevatedButton(
+                          child: const Text("画像を再生成"),
+                          onPressed: () async {
+                            makeImage(0);
+                          },
+                        ),
+                        //サムネイルURLがそもそも存在しないならオプションを表示しない
+                        if (widget.bookData.coverUrl != "")
+                          SwitchListTile(
+                            title: const Text('サムネイルを表示'),
+                            value: isThumbnail,
+                            onChanged: ((newVal) {
+                              setState(() {
+                                isThumbnail = newVal;
+                              });
+                              makeImage(0);
+                            }),
+                            secondary: const Icon(Icons.image_rounded),
+                          )
+                      ],
                 ))));
+  }
+
+  List<Widget> buildAfterGeneration() {
+    if (_bytes == null) return [];
+    return [
+      LayoutBuilder(builder: (context, size) {
+        return ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 500),
+            child: Container(
+              padding: const EdgeInsets.all(32),
+              width: double.infinity,
+              height: size.maxWidth / 1.775,
+              child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Image.memory(_bytes!.buffer.asUint8List())),
+            ));
+      }),
+      ElevatedGradationButton(
+          onPressed: () async {
+            //ダブルクリック防止
+            if (_bytes != null && saveStatus != ProcessingStatus.processing) {
+              setState(() {
+                saveStatus = ProcessingStatus.processing;
+              });
+              await ImageGallerySaver.saveImage(_bytes!);
+              setState(() {
+                saveStatus = ProcessingStatus.processed;
+              });
+            }
+          },
+          gradient: const LinearGradient(
+            colors: [Colors.blueAccent, Colors.purple],
+          ),
+          child: RichText(
+            text: const TextSpan(
+              children: [
+                WidgetSpan(
+                  child: Icon(Icons.save_alt),
+                ),
+                TextSpan(
+                    text: 'アルバムに保存',
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          )),
+      if (saveStatus == ProcessingStatus.processed)
+        Text(
+          "保存しました",
+          style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+        ),
+      const SizedBox(height: 15),
+      const Text("または"),
+      const SizedBox(height: 15),
+      ElevatedGradationButton(
+          onPressed: () async {
+            //ダブルクリック防止
+            if (_bytes != null && shareStatus != ProcessingStatus.processing) {
+              setState(() {
+                shareStatus = ProcessingStatus.processing;
+              });
+              //共有シートを開いてる時にもう一度呼ぶとクラッシュする
+              try {
+                await Share.shareXFiles(
+                    [XFile.fromData(_bytes!, mimeType: 'image/png')]);
+              } catch (err) {}
+              setState(() {
+                shareStatus = ProcessingStatus.processed;
+              });
+            }
+          },
+          gradient: const LinearGradient(
+            colors: [
+              Colors.purple,
+              Colors.redAccent,
+            ],
+          ),
+          child: RichText(
+            text: const TextSpan(
+              children: [
+                WidgetSpan(
+                  child: Icon(Icons.ios_share),
+                ),
+                TextSpan(
+                    text: 'カードを共有',
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          )),
+      const SizedBox(height: 35.0),
+      if (_bytes != null) const Divider(),
+    ];
   }
 
   void makeImage(int waitSeconds) async {
@@ -87,3 +157,5 @@ class _CardGenerationPageState extends State<CardGenerationPage> {
     setState(() {});
   }
 }
+
+enum ProcessingStatus { unprocessed, processing, processed }
